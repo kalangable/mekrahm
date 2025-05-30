138a426d84c7d2e8fea0eb4c53ce6264b38100b9
Adilson Krasovski <adilson.krasovski@neogrid.com>
2025-05-30 01:28:48 -0300

    ajustando o arquivo para carregar as informações iniciais

--- src/main/resources/data/products-data.sql
+++ src/main/resources/data/products-data.sql
@@ -1,13 +1,13 @@
-INSERT INTO product (id, ean, description) VALUES
-    (1, '7891234567890', 'Notebook Gamer Ryzen 7'),
-    (2, '7899876543210', 'Smartphone Android 5G'),
-    (3, '7894561237890', 'Headset Bluetooth Noise Cancelling'),
-    (4, '7893216549870', 'Teclado Mecânico RGB ABNT2'),
-    (5, '7897412589630', 'Monitor 27" Full HD IPS');
+INSERT INTO product (ean, description) VALUES
+    ('7891234567890', 'Notebook Gamer Ryzen 7'),
+    ('7899876543210', 'Smartphone Android 5G'),
+    ('7894561237890', 'Headset Bluetooth Noise Cancelling'),
+    ('7893216549870', 'Teclado Mecânico RGB ABNT2'),
+    ('7897412589630', 'Monitor 27" Full HD IPS');
 
-INSERT INTO product (id, ean, description) VALUES
-    (6,  '7890000000001', 'Produto A'),
-    (7,  '7890000000002', 'Produto B'),
-    (8,  '7890000000003', 'Produto C'),
-    (9,  '7890000000004', 'Produto D'),
-    (10, '7890000000005', 'Produto E');
+INSERT INTO product (ean, description) VALUES
+    ('7890000000001', 'Produto A'),
+    ('7890000000002', 'Produto B'),
+    ('7890000000003', 'Produto C'),
+    ('7890000000004', 'Produto D'),
+    ('7890000000005', 'Produto E');
